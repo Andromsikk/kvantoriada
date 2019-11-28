65073817ceb6b8cc8a2c745ef7172faffa7d0f8b
Andromsikk <igolkaman@gmail.com>
2019-11-28 23:02:32 +0300

    добавление фаилов

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onload = init;
+
+function init(){
+    var button = document.getElementById("addButton")
+    button.onclick = handleButtonClick;
+}
+
+function handleButtonClick() {
+    alert("Вы нажали на кнопку");
+}
