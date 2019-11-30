1fdd7b8f4dab6f7dce91728d775aaa9952db65c4
Andromsikk <igolkaman@gmail.com>
2019-11-30 23:28:44 +0300

    созданан карточка с выпадающими менюшками

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,121 +1,109 @@
 <template>
-    <v-card>
-        <v-card-title>
-            List of polls
-            <v-spacer></v-spacer>
-            <v-text-field
-                    v-model="search"
-                    append-icon=""
-                    label="Search"
-                    single-line
-                    hide-details
-            ></v-text-field>
-        </v-card-title>
-        <v-data-table
-                :headers="headers"
-                :items="desserts"
-                :search="search"
-        ></v-data-table>
-    </v-card>
+  <v-card
+    class="mx-auto my-10"
+    max-width="400"
+  >
+    <v-img
+      src="https://cdn.pixabay.com/photo/2015/04/23/17/18/disney-736374__480.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      ВЫ: Сцена
+    </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    <v-card-actions>
+         <v-card-text class="py-0">
+        <h3>Ваш тариф</h3> 
+        </v-card-text>
+        
+
+        <v-spacer></v-spacer>
+        <v-btn
+            icon
+            @click="show = !show"
+        >
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
+    </v-card-actions>
+
+
+   
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+        <v-card-text>
+          I'm a thing. But
+        </v-card-text>
+            </div>
+    </v-expand-transition>
+
+    <v-card-actions>
+          <v-card-text class="py-0">
+        <h3>Счет на Оплату</h3> 
+        </v-card-text>
+        
+         <v-spacer></v-spacer>
+        <v-btn
+                icon
+                @click="show2 = !show2"
+            >
+                <v-icon>{{ show2 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+    </v-card-actions>
+
+
+
+    <v-expand-transition>
+        <div v-show="show2">
+        <v-divider></v-divider>
+        <v-card-text>
+        I'm a thing. But
+        </v-card-text>
+        </div>
+    </v-expand-transition>
+
+    <v-card-actions>
+          <v-card-text class="py-0">
+        <h3> Счета Квитанции</h3> 
+        </v-card-text>
+        
+         <v-spacer></v-spacer>
+        <v-btn
+                icon
+                @click="show3 = !show3"
+            >
+                <v-icon>{{ show3 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+    </v-card-actions>
+
+     <v-expand-transition>
+        <div v-show="show3">
+        <v-divider></v-divider>
+        <v-card-text>
+        I'm a thing. But
+        </v-card-text>
+        </div>
+    </v-expand-transition>
+    
+
+
+
+
+  </v-card>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                search: '',
-                headers: [
-                    {
-                        text: 'Poll title',
-                        align: 'left',
-                        sortable: false,
-                        value: 'name',
-                    },
-                    {text: 'Company', value: 'company'},
-                    {text: 'Number of questions', value: 'numquest'},
-                    {text: 'Polled users', value: 'userpolls'},
-                    {text: 'Creation date', value: 'creatdate'},
-                ],
-                desserts: [
-                    {
-                        name: 'Motor mechanic',
-                        company: 'Roga I Kopita',
-                        userpolls: 159,
-                        numquest: 7,
-                        creatdate: '02.10.2015',
-                    },
-                    {
-                        name: 'Obstetrician',
-                        company: 'Westindische Compagnie',
-                        userpolls: 237,
-                        numquest: 12,
-                        creatdate: '22.09.2017',
-                    },
-                    {
-                        name: 'Auditor',
-                        company: 'Roga I Kopita',
-                        userpolls: 262,
-                        numquest: 10,
-                        creatdate: '28.12.2016',
-                    },
-                    {
-                        name: 'Botanist',
-                        company: 'Westindische Compagnie',
-                        userpolls: 305,
-                        numquest: 3,
-                        creatdate: '16.12.2019',
-                    },
-                    {
-                        name: 'Bookkeeper',
-                        company: 'Westindische Compagnie',
-                        userpolls: 356,
-                        numquest: 15,
-                        creatdate: '15.04.2016',
-                    },
-                    {
-                        name: 'Plumber',
-                        company: 'Roga I Kopita',
-                        userpolls: 375,
-                        numquest: 26,
-                        creatdate: '19.12.2017',
-                    },
-                    {
-                        name: 'Geneticist',
-                        company: 'Roga I Kopita',
-                        userpolls: 392,
-                        numquest: 44,
-                        creatdate: '10.11.2016',
-                    },
-                    {
-                        name: 'Railwayman',
-                        company: 'Roga I Kopita',
-                        userpolls: 408,
-                        numquest: 35,
-                        creatdate: '30.11.2015',
-                    },
-                    {
-                        name: 'Merchant',
-                        company: 'Westindische Compagnie',
-                        userpolls: 452,
-                        numquest: 17,
-                        creatdate: '27.03.2017',
-                    },
-                    {
-                        name: 'Astronaut',
-                        company: 'Roga I Kopita',
-                        userpolls: 518,
-                        numquest: 18,
-                        creatdate: '29.03.2016',
-                    },
-                    {
-                        name: 'Astronaut',
-                        company: 'Roga I Kopita',
-                        userpolls: 518,
-                        numquest: 18,
-                        creatdate: '29.03.2016',
-                    },
-                ],
-            }
-        },
-    }
+  export default {
+    data: () => ({
+      show: false,
+      show2: false,
+      show3: false,
+    }),
+  }
 </script>
