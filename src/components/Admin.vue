e1da1f82562013c0a772e3860ce9eaff16a9024e
Andromsikk <igolkaman@gmail.com>
2019-12-01 00:46:33 +0300

    Добавлены все тач бары, сломалась вылезающая менюшка

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -12,9 +12,38 @@
       ВЫ: Сцена
     </v-card-title>
 
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
+   
+    <section>
+        <v-container fluid class="d-flex mb-0 pb-0">
+            <h3 class="pl-3">Подача тока</h3>
+            <v-switch v-model="switch1" :label="``" class="ma-0 ml-3 pa-0"></v-switch> on
+            
+        </v-container>
+        
+          
+
+        <v-col class="d-flex ma-0 pa-0 algin-center" cols="12" sm="8">
+            <h3 class=" pl-6">Трек:</h3>
+            <v-select
+            :items="items"
+            label="Выберети музыку"
+            class="ma-0 pa-0 ml-3 "
+            ></v-select>
+        </v-col>
+
+        <v-container fluid class="d-flex mb-0 pb-0">
+            <h3 class="pl-3">Громкость:</h3>
+             
+               
+                <v-slider
+                v-model="slider"
+                thumb-label
+                class="pl-1 ma-0"
+                ></v-slider>
+              
+        </v-container>
+    </section>
+
 
     <v-card-actions>
          <v-card-text class="py-0">
@@ -70,7 +99,7 @@
 
     <v-card-actions>
           <v-card-text class="py-0">
-        <h3> Счета Квитанции</h3> 
+        <h3>Счета Квитанции</h3> 
         </v-card-text>
         
          <v-spacer></v-spacer>
@@ -107,3 +136,24 @@
     }),
   }
 </script>
+
+<script>
+  export default {
+    data () {
+      return {
+        switch1: false,
+        switch2: false,
+      }
+    },
+  }
+</script>
+
+
+<script>
+  export default {
+    data: () => ({
+      items: ['Человечская музыка', 'Шрек musik', 'Jazz', 'Rock'],
+    }),
+  }
+</script>
+
